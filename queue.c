b5ef9da1c3dc48383c5a023847080e560c2c53fd
blueskyson <blueskyson1401@gmail.com>
2020-09-19 02:33:57 +0800

    Finish all except q_reverse and q_sort

--- queue.c
+++ queue.c
@@ -5,86 +5,110 @@
 #include "harness.h"
 #include "queue.h"
 
-/*
- * Create empty queue.
- * Return NULL if could not allocate space.
- */
 queue_t *q_new()
 {
     queue_t *q = malloc(sizeof(queue_t));
-    /* TODO: What if malloc returned NULL? */
+    if (!q)
+        return NULL;
     q->head = NULL;
+    q->tail = NULL;
+    q->size = 0;
     return q;
 }
 
 /* Free all storage used by queue */
 void q_free(queue_t *q)
 {
-    /* TODO: How about freeing the list elements and the strings? */
-    /* Free queue structure */
+    if (!q)
+        return;
+    while (q->head) {
+        list_ele_t *del = q->head;
+        q->head = q->head->next;
+        free(del->value);
+        free(del);
+    }
     free(q);
 }
 
-/*
- * Attempt to insert element at head of queue.
- * Return true if successful.
- * Return false if q is NULL or could not allocate space.
- * Argument s points to the string to be stored.
- * The function must explicitly allocate space and copy the string into it.
- */
 bool q_insert_head(queue_t *q, char *s)
 {
-    list_ele_t *newh;
-    /* TODO: What should you do if the q is NULL? */
-    newh = malloc(sizeof(list_ele_t));
-    /* Don't forget to allocate space for the string and copy it */
-    /* What if either call to malloc returns NULL? */
+    if (!q)
+        return false;
+    list_ele_t *newh = malloc(sizeof(list_ele_t));
+    if (!newh)
+        return false;
+    unsigned len = strlen(s) + 1;
+    char *val = malloc(sizeof(char) * len);
+    if (!val) {
+        free(newh);
+        return false;
+    }
+    memcpy(val, s, len);
+    newh->value = val;
     newh->next = q->head;
     q->head = newh;
+    if (q->size == 0)
+        q->tail = newh;
+    (q->size)++;
     return true;
 }
 
-/*
- * Attempt to insert element at tail of queue.
- * Return true if successful.
- * Return false if q is NULL or could not allocate space.
- * Argument s points to the string to be stored.
- * The function must explicitly allocate space and copy the string into it.
- */
 bool q_insert_tail(queue_t *q, char *s)
 {
-    /* TODO: You need to write the complete code for this function */
-    /* Remember: It should operate in O(1) time */
-    /* TODO: Remove the above comment when you are about to implement. */
-    return false;
-}
-
-/*
- * Attempt to remove element from head of queue.
- * Return true if successful.
- * Return false if queue is NULL or empty.
- * If sp is non-NULL and an element is removed, copy the removed string to *sp
- * (up to a maximum of bufsize-1 characters, plus a null terminator.)
- * The space used by the list element and the string should be freed.
- */
-bool q_remove_head(queue_t *q, char *sp, size_t bufsize)
-{
-    /* TODO: You need to fix up this code. */
-    /* TODO: Remove the above comment when you are about to implement. */
-    q->head = q->head->next;
+    if (q == NULL)
+        return false;
+    list_ele_t *newt = malloc(sizeof(list_ele_t));
+    if (!newt)
+        return false;
+    unsigned len = strlen(s) + 1;
+    char *val = malloc(sizeof(char) * len);
+    if (!val) {
+        free(newt);
+        return false;
+    }
+    memcpy(val, s, len);
+    newt->value = val;
+    newt->next = NULL;
+    if (q->size == 0) {
+        q->head = newt;
+        q->tail = newt;
+    } else {
+        q->tail->next = newt;
+        q->tail = newt;
+    }
+    (q->size)++;
     return true;
 }
 
-/*
- * Return number of elements in queue.
- * Return 0 if q is NULL or empty
- */
+bool q_remove_head(queue_t *q, char *sp, size_t bufsize)
+{
+    if (!q || !(q->head))
+        return false;
+    if (q->size == 1)
+        q->tail = NULL;
+    list_ele_t *del;
+    del = q->head;
+    q->head = q->head->next;
+
+    if (sp) {
+        if (strlen(del->value) > bufsize) {
+            memcpy(sp, del->value, bufsize);
+            sp[bufsize - 1] = '\0';
+        } else {
+            memcpy(sp, del->value, sizeof(char) * (strlen(del->value) + 1));
+        }
+    }
+    free(del->value);
+    free(del);
+    (q->size)--;
+    return true;
+}
+
 int q_size(queue_t *q)
 {
-    /* TODO: You need to write the code for this function */
-    /* Remember: It should operate in O(1) time */
-    /* TODO: Remove the above comment when you are about to implement. */
-    return 0;
+    if (!q)
+        return 0;
+    return q->size;
 }
 
 /*
--- queue.h
+++ queue.h
@@ -1,34 +1,19 @@
 #ifndef LAB0_QUEUE_H
 #define LAB0_QUEUE_H
 
-/*
- * This program implements a queue supporting both FIFO and LIFO
- * operations.
- *
- * It uses a singly-linked list to represent the set of queue elements
- */
-
 #include <stdbool.h>
 #include <stddef.h>
 
-/* Data structure declarations */
-
-/* Linked list element (You shouldn't need to change this) */
 typedef struct ELE {
-    /* Pointer to array holding string.
-     * This array needs to be explicitly allocated and freed
-     */
     char *value;
     struct ELE *next;
 } list_ele_t;
 
 /* Queue structure */
 typedef struct {
-    list_ele_t *head; /* Linked list of elements */
-    /* TODO: You will need to add more fields to this structure
-     *        to efficiently implement q_size and q_insert_tail.
-     */
-    /* TODO: Remove the above comment when you are about to implement. */
+    list_ele_t *head;
+    list_ele_t *tail;
+    unsigned size;
 } queue_t;
 
 /* Operations on queue */
